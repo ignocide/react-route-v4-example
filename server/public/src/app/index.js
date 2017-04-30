652c0ae5171807d6522fdc876b54aeda088b042d
sungmin kim <ignocide@gmail.com>
2017-05-01 02:37:13 +0900

    * apply skeleton css frame * fix a package.json about name, desc

--- server/public/src/app/index.js
+++ server/public/src/app/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import App from './components/App'
 
-import '../scss/main.scss'
+import '../scss/bundle.scss'
 
 ReactDOM.render(
   <div>
-    <App></App>
+    <App />
   </div>, document.getElementById('react-root')
 )
